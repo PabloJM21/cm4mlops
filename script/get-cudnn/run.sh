c6be998483bc5eb193f2577284fa4518612518ef
PabloJM21 <pablo.jahnen@stud.uni-goettingen.de>
2024-12-30 18:12:27 +0100

    Update run.sh

--- script/get-cudnn/run.sh
+++ script/get-cudnn/run.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 if [ ${CM_TMP_RUN_COPY_SCRIPT} == "yes" ]; then
-    echo ${CM_TMP_INC_PATH}
-    echo ${CM_CUDA_PATH_INCLUDE}
+
+    echo ${CM_TMP_INC_PATH} > paths.txt
+    echo ${CM_CUDA_PATH_INCLUDE} >> paths.txt
+
     cmd="$cp   ${CM_TMP_INC_PATH}/*.h   ${CM_CUDA_PATH_INCLUDE}/"
     echo $cmd
     eval $cmd
